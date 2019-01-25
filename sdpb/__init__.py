943ba8ec6c8377cf97dbb06f40edf684790fe669
Rod Glover <rglover@uvic.ca>
2019-01-25 12:31:31 -0800

    Disable SQLALCHEMY_TRACK_MODIFICATIONS

--- sdpb/__init__.py
+++ sdpb/__init__.py
@@ -7,7 +7,9 @@ from sdpb.routes import add_routes
 def get_app(config_override={}):
     app = Flask(__name__)
     CORS(app)
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('PCDS_DSN', 'postgresql://httpd@monsoon.pcic.uvic.ca/crmp')
+    app.config['SQLALCHEMY_DATABASE_URI'] = \
+        os.getenv('PCDS_DSN', 'postgresql://httpd@monsoon.pcic.uvic.ca/crmp')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config.update(config_override)
     add_routes(app)
     return app
