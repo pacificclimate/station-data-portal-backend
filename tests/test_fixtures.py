c8123d3f2d3843708819938540cce38a24338e6a
Rod Glover <rglover@uvic.ca>
2019-01-25 12:31:55 -0800

    Clean up some tests

--- tests/test_fixtures.py
+++ tests/test_fixtures.py
@@ -1,15 +1,8 @@
+# Just makin' sure all the fixtures are copacetic.
+
 from pycds import Station
 
 
 def test_stations(station_session, tst_stations):
     stations = station_session.query(Station).all()
     assert len(stations) == len(tst_stations)
-
-    print('\nstations from test fixture')
-    for stn in tst_stations:
-        print('id {id}; {native_id}'.format(**stn.__dict__))
-
-    print('\nstations from db query')
-    for stn in stations:
-        print('id {id}; {native_id}'.format(**stn.__dict__))
-
