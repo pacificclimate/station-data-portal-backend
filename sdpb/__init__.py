8446fe7d8e6a3db29139aa285deade3b5a4b7be5
Rod Glover <rglover@uvic.ca>
2019-01-25 16:51:16 -0800

    Use db3

--- sdpb/__init__.py
+++ sdpb/__init__.py
@@ -8,7 +8,7 @@ def get_app(config_override={}):
     app = Flask(__name__)
     CORS(app)
     app.config['SQLALCHEMY_DATABASE_URI'] = \
-        os.getenv('PCDS_DSN', 'postgresql://httpd@monsoon.pcic.uvic.ca/crmp')
+        os.getenv('PCDS_DSN', 'postgresql://httpd@db3.pcic.uvic.ca/crmp')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config.update(config_override)
     add_routes(app)
