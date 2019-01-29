46290d2df9c0ee1347e77d3592492691e34c8508
Rod Glover <rglover@uvic.ca>
2019-01-28 16:22:42 -0800

    Use new history interface (faster)

--- sdpb/api/stations.py
+++ sdpb/api/stations.py
@@ -1,7 +1,14 @@
-from pycds import Station
+import logging
+from flask.logging import default_handler
+from pycds import Station, VarsPerHistory
 from sdpb.api.networks import network_uri
-from sdpb.api.histories import history_rep
-from sdpb.util import date_rep
+from sdpb.api.histories import history_collection_rep, history_rep
+from sdpb.util import date_rep, get_all_vars_by_hx
+
+
+logger = logging.getLogger(__name__)
+logger.addHandler(default_handler)
+logger.setLevel(logging.DEBUG)
 
 
 def station_uri(station):
@@ -9,8 +16,9 @@ def station_uri(station):
     return '/stations/{}'.format(station.id)
 
 
-def station_rep(station):
+def station_rep(station, all_vars_by_hx):
     """Return representation of a single station item."""
+    logger.debug('station_rep id: {}'.format(station.id))
     return {
         'id': station.id,
         'uri': station_uri(station),
@@ -18,29 +26,40 @@ def station_rep(station):
         'min_obs_time': date_rep(station.min_obs_time),
         'max_obs_time': date_rep(station.max_obs_time),
         'network_uri': network_uri(station.network),
-        'histories': [history_rep(history) for history in station.histories]
+        'histories': history_collection_rep(station.histories, all_vars_by_hx)
     }
 
 
 def get_station_item_rep(session, id=None):
     assert id is not None
+    logger.debug('get station')
     station = session.query(Station).filter_by(id=id).one()
-    return station_rep(station)
+    logger.debug('station retrieved')
+    all_vars_by_hx = get_all_vars_by_hx(session)
+    return station_rep(station, all_vars_by_hx)
 
 
-def station_collection_item_rep(station):
+def station_collection_item_rep(station, all_vars_by_hx):
     """Return representation of a station collection item.
     May conceivably be different than representation of a single a station.
     """
-    return station_rep(station)
+    return station_rep(station, all_vars_by_hx)
 
 
-def station_collection_rep(stations):
+def station_collection_rep(stations, all_variables):
     """Return representation of stations collection. """
-    return [station_collection_item_rep(station) for station in stations]
+    return [station_collection_item_rep(station, all_variables) for station in stations]
 
 
 def get_station_collection_rep(session):
     """Get stations from database, and return their representation."""
-    stations = session.query(Station).order_by(Station.id.asc()).all()
-    return station_collection_rep(stations)
+    logger.debug('get stations')
+    stations = (
+        session.query(Station)
+        .order_by(Station.id.asc())
+            .limit(1000)
+            .all()
+    )
+    logger.debug('stations retrieved')
+    all_vars_by_hx = get_all_vars_by_hx(session)
+    return station_collection_rep(stations, all_vars_by_hx)
