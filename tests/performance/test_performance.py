21d3944d97b648bd9177a1a9e505859ef0a39c3f
rod-glover <rod.glover.ca@gmail.com>
2022-04-08 09:11:46 -0700

    Update performance tests

--- tests/performance/test_performance.py
+++ tests/performance/test_performance.py
@@ -1,5 +1,11 @@
 import pytest
-from sdpb.api import networks
+from sdpb.api import (
+    crmp_network_geoserver,
+    networks,
+    variables,
+    histories,
+    stations,
+)
 from sdpb.timing import timing
 
 
@@ -7,6 +13,33 @@ from sdpb.timing import timing
 pytestmark = pytest.mark.usefixtures("flask_app")
 
 
-def test_networks():
-    t = timing(networks.list)
-    print(f"List networks: elapsed {t['elapsed']}")
+multiplier = 1000
+units = "ms"
+
+
+def print_timing(f, label, **kwargs):
+    t = timing(
+        histories.list, **kwargs
+    )
+    print("")
+    print(
+        f"{label}"
+        f"({kwargs}): "
+        f"{len(t['value'])} items; elapsed {t['elapsed'] * 1000} {units}"
+    )
+
+
+@pytest.mark.parametrize("compact", [False, True])
+@pytest.mark.parametrize("group_vars_in_database", [False, True])
+def test_histories_list(compact, group_vars_in_database):
+    print_timing(
+        histories.list,
+        "histories",
+        # province="BC",
+        compact=compact,
+        group_vars_in_database=group_vars_in_database,
+    )
+
+
+def test_crmp_network_geoserver():
+    print_timing(crmp_network_geoserver, "crmp_network_geoserver")
