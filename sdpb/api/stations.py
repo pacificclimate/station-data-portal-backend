dd822ce9e93b7c1aeff999755c95eb1a4e844ef1
rod-glover <rod.glover.ca@gmail.com>
2022-04-08 14:21:51 -0700

    Add expand param to stations list

--- sdpb/api/stations.py
+++ sdpb/api/stations.py
@@ -7,6 +7,7 @@ history items (q.v.).
 """
 import logging
 from flask import url_for
+from sqlalchemy import func
 from pycds import Network, Station, History, StationObservationStats
 from sdpb import get_app_session
 from sdpb.api import networks
@@ -25,18 +26,26 @@ logger = logging.getLogger("sdpb")
 # TODO: Name function parameters consistently
 
 
+def is_expanded(item, expand):
+    return expand is not None and expand == item
+
+
 def uri(station):
     """Return uri for a station"""
     return url_for("sdpb_api_stations_get", id=station.id)
 
 
 def single_item_rep(
-    station, station_histories_etc, all_vars_by_hx=None, compact=False
+    station_etc,
+    station_histories_etc,
+    all_vars_by_hx=None,
+    compact=False,
+    expand=None,
 ):
     """
     Return a representation of a single station item.
 
-    :param station: Database result containing a Station.
+    :param station_etc: Database result containing a Station.
     :param station_histories_etc: Iterable containing histories etc.,
         associated with the station. For definition of histories etc.,
         see histories module.
@@ -47,16 +56,26 @@ def single_item_rep(
     """
     """"""
     set_logger_level_from_qp(logger)
+    expand_histories = is_expanded("histories", expand)
+
+    station = getattr(station_etc, "Station", station_etc)
+
+    if expand_histories:
+        histories_rep = histories.collection_rep(
+            station_histories_etc,
+            all_vars_by_hx=all_vars_by_hx,
+            compact=compact,
+        )
+    else:
+        histories_rep = [
+            {"uri": histories.uri(hx_id)} for hx_id in station_etc.history_ids
+        ]
     rep = {
         "id": station.id,
         "uri": uri(station),
         "native_id": station.native_id,
         "network_uri": networks.uri(station.network),
-        "histories": histories.collection_rep(
-            station_histories_etc,
-            all_vars_by_hx=all_vars_by_hx,
-            compact=compact,
-        ),
+        "histories": histories_rep,
     }
     if compact:
         return rep
@@ -68,7 +87,11 @@ def single_item_rep(
 
 
 def collection_item_rep(
-    station, station_histories_etc, all_vars_by_hx=None, compact=False
+    station,
+    station_histories_etc,
+    all_vars_by_hx=None,
+    compact=False,
+    expand=None,
 ):
     """
     Return representation of a station collection item.
@@ -90,11 +113,16 @@ def collection_item_rep(
         station_histories_etc,
         all_vars_by_hx=all_vars_by_hx,
         compact=compact,
+        expand=expand,
     )
 
 
 def collection_rep(
-    stations, all_histories_etc_by_station, all_vars_by_hx=None, compact=False
+    stations,
+    all_histories_etc_by_station,
+    all_vars_by_hx=None,
+    compact=False,
+    expand=None,
 ):
     """
     Return a representation of a stations collection.
@@ -107,16 +135,24 @@ def collection_rep(
         keyed by history id.
     :param compact: Boolean. Return compact or full representation of each
         station.
+    :param expand:
     :return: dict
     """
 
-    def histories_etc_for(station):
+    # TODO: Consider always getting history id's in station query and doing
+    #  in-code join on that basis rather than using station id and histories
+    #  grouped by station. Query for histories may be faster that way.
+
+    def histories_etc_for(station_etc):
+        station = getattr(station_etc, "Station", station_etc)
         with log_timing(
             f"histories_etc_for {station.id}",
             # logging this makes the service very very slow
             # log=logger.debug,
             log=None,
         ):
+            if all_histories_etc_by_station is None:
+                return None
             try:
                 # result = station.histories
                 result = all_histories_etc_by_station[station.id]
@@ -132,6 +168,7 @@ def collection_rep(
             histories_etc_for(station),
             all_vars_by_hx=all_vars_by_hx,
             compact=compact,
+            expand=expand,
         )
         for station in stations
     ]
@@ -169,8 +206,9 @@ def list(
     limit=None,
     offset=None,
     province=None,
-    compact=False,
+    compact=True,
     group_vars_in_database=True,
+    expand="histories",
 ):
     """
     Get stations from database, and return their representation.
@@ -183,20 +221,27 @@ def list(
     :param compact: Boolean. Return compact rep?
     :param group_vars_in_database: Boolean. Group variables by history id in
         database or in code?
-    :return:
+    :param expand: Associated items to expand. Valid values: "histories".
+    :return: list of dict
     """
     set_logger_level_from_qp(logger)
     logger.debug(
-        f"stations.list(stride={stride}, limit={limit}, offset={offset}, compact={compact})"
+        f"stations.list(stride={stride}, limit={limit}, offset={offset}, "
+        f"compact={compact}, expand={expand})"
     )
     session = get_app_session()
+    expand_histories = is_expanded("histories", expand)
 
     with log_timing("List all stations", log=logger.debug):
         with log_timing("Query all stations", log=logger.debug):
+            if expand_histories:
+                q = session.query(Station).select_from(Station)
+            else:
+                q = session.query(
+                    Station, func.array_agg(History.id).label("history_ids")
+                ).group_by(Station.id)
             q = (
-                session.query(Station)
-                .select_from(Station)
-                .join(Network, Station.network_id == Network.id)
+                q.join(Network, Station.network_id == Network.id)
                 .filter(Network.publish == True)
                 .order_by(Station.id.asc())
             )
@@ -212,7 +257,11 @@ def list(
                 )
             stations = q.all()
 
-        all_histories_etc_by_station = get_all_histories_etc_by_station(session)
+        all_histories_etc_by_station = (
+            get_all_histories_etc_by_station(session)
+            if expand_histories
+            else None
+        )
         all_vars_by_hx = get_all_vars_by_hx(
             session, group_in_database=group_vars_in_database
         )
@@ -223,4 +272,5 @@ def list(
                 all_histories_etc_by_station,
                 all_vars_by_hx=all_vars_by_hx,
                 compact=compact,
+                expand=expand,
             )
