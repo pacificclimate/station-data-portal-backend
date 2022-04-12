cb0fba2557480c28e604a7f44ad876b6bd329212
rod-glover <rod.glover.ca@gmail.com>
2022-04-12 16:37:13 -0700

    Improve stations unit tests

--- tests/unit/api/test_stations.py
+++ tests/unit/api/test_stations.py
@@ -1,7 +1,10 @@
+import pprint
+
+import pytest
 from pycds import Station
 from sdpb.api import networks, stations
 from sdpb.util.representation import date_rep
-from helpers import groupby_dict
+from helpers import groupby_dict, omit
 from test_histories import expected_history_rep
 
 
@@ -10,28 +13,52 @@ def test_stations_uri(flask_app):
     assert stations.uri(station) == "http://test/stations/99"
 
 
-def expected_station_rep(station, all_histories, all_stn_obs_stats, vars_by_hx):
+def expected_station_rep(
+    station,
+    all_histories,
+    all_stn_obs_stats,
+    vars_by_hx,
+    compact=False,
+    expand=None,
+):
     hxs_by_station_id = groupby_dict(all_histories, key=lambda h: h.station_id)
     try:
         stn_histories = hxs_by_station_id[station.id]
     except KeyError:
         stn_histories = []
 
-    return {
+    histories_rep = [
+        expected_history_rep(hx, all_stn_obs_stats, vars_by_hx, compact=compact)
+        for hx in stn_histories
+    ]
+    if "histories" not in (expand or "").split(","):
+        histories_rep = [{"uri": hr["uri"]} for hr in histories_rep]
+    rep = {
         "id": station.id,
         "uri": stations.uri(station),
         "native_id": station.native_id,
+        "network_uri": networks.uri(station.network),
+        "histories": histories_rep,
+    }
+    if compact:
+        return rep
+    return {
+        **rep,
         "min_obs_time": date_rep(station.min_obs_time),
         "max_obs_time": date_rep(station.max_obs_time),
-        "network_uri": networks.uri(station.network),
-        "histories": [
-            expected_history_rep(hx, all_stn_obs_stats, vars_by_hx)
-            for hx in stn_histories
-        ],
     }
 
 
-def test_station_collection_hx_omit_vars(
+@pytest.mark.parametrize("compact", [False, True])
+@pytest.mark.parametrize("group_vars_in_database", [False, True])
+@pytest.mark.parametrize(
+    "expand",
+    [
+        None,
+        "histories"
+    ],
+)
+def test_station_collection(
     flask_app,
     everything_session,
     tst_networks,
@@ -39,13 +66,46 @@ def test_station_collection_hx_omit_vars(
     tst_histories,
     tst_stn_obs_stats,
     tst_vars_by_hx,
+    compact,
+    group_vars_in_database,
+    expand,
 ):
-    stn_reps = sorted(stations.list(compact=False), key=lambda r: r["id"])
-    expected = [
-        expected_station_rep(
-            station, tst_histories, tst_stn_obs_stats, tst_vars_by_hx
+    received = sorted(
+        stations.list(compact=compact, expand=expand), key=lambda r: r["id"]
+    )
+    expected = sorted(
+        [
+            expected_station_rep(
+                station,
+                tst_histories,
+                tst_stn_obs_stats,
+                tst_vars_by_hx,
+                compact=compact,
+                expand=expand,
+            )
+            for station in tst_stations
+            if station.network.publish is True and len(station.histories) > 0
+        ],
+        key=lambda r: r["id"],
+    )
+    # print()
+    # print("### received")
+    # pprint.pprint(received)
+    # print()
+    # print("### expected")
+    # pprint.pprint(expected)
+    expand_histories = expand == "histories"
+    assert len(received) == len(expected)
+    for r, e in zip(received, expected):
+        assert omit(e, ["histories"]) == omit(r, ["histories"])
+        # Histories have no order imposed, so it's a bit trickier to check them.
+        ehs = e["histories"]
+        rhs = r["histories"]
+        # Check they agree on which histories
+        assert {h["uri"] for h in ehs} == {h["uri"] for h in rhs}
+        # Check they agree on compact
+        assert {len(h) for h in ehs} == {len(h) for h in rhs}
+        # Check they agree on expanded
+        assert all(
+            (len(rh) > 1) if expand_histories else (len(rh) == 1) for rh in rhs
         )
-        for station in tst_stations
-        if station.network_id == tst_networks[0].id
-    ]
-    assert stn_reps == expected
