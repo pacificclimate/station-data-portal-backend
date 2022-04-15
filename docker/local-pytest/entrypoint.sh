c6223d797835902a49c0d5a88d5f2de57c644e2c
rod-glover <rod.glover.ca@gmail.com>
2022-04-14 17:05:34 -0700

    Update local-pytest

--- docker/local-pytest/entrypoint.sh
+++ docker/local-pytest/entrypoint.sh
@@ -1,3 +1,6 @@
-# Install SDP backend
+# Note: we are in directory ${INSTALL_DIR}
+cp "${CODEBASE_DIR}"/Pipfile .
+# Point copied Pipfile at /codebase
+sed -i '/sdpb/s|path = "."|path = "/codebase"|' Pipfile
 pipenv install --dev
 /bin/bash
