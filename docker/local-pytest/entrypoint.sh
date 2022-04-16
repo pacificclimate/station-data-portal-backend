2262c95c47f2f05c76cadbbdcfc5a06dcc7f1682
rod-glover <rod.glover.ca@gmail.com>
2022-04-15 17:20:59 -0700

    Update local-pytest ... again

--- docker/local-pytest/entrypoint.sh
+++ docker/local-pytest/entrypoint.sh
@@ -1,6 +1,7 @@
+#!/bin/bash
 # Note: we are in directory ${INSTALL_DIR}
-cp "${CODEBASE_DIR}"/Pipfile .
+cp -r "${CODEBASE_DIR}"/* "${INSTALL_DIR}"
 # Point copied Pipfile at /codebase
-sed -i '/sdpb/s|path = "."|path = "/codebase"|' Pipfile
+# sed -i '/sdpb/s|path = "."|path = "/codebase"|' Pipfile
 pipenv install --dev
 /bin/bash
