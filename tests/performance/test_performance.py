d6f51763cc2bd18fda9f068705f508aa88aa0913
rod-glover <rod.glover.ca@gmail.com>
2022-04-19 16:08:23 -0700

    Update perf tests

--- tests/performance/test_performance.py
+++ tests/performance/test_performance.py
@@ -1,6 +1,7 @@
 import datetime
 import math
-from statistics import mean, stdev
+from itertools import product
+from statistics import mean
 import os
 import pprint
 import pytest
@@ -67,6 +68,26 @@ def print_timing(f, label, print_example=False, **kwargs):
 #     print(f"repeats = {repeats}")
 
 
+boolean = (False, True)
+
+
+def dict_iter(*spec):
+    """
+    spec = tuple of (key_name, key_iterable)
+    return iterable of dicts with keys key_name over cartesian product of
+    iterables.
+    :param spec:
+    :return:
+    """
+    names = tuple(item[0] for item in spec)
+    iterables = tuple(item[1] for item in spec)
+    prod = product(*iterables)
+    return (
+        {name: value for name, value in zip(names, values)}
+        for values in prod
+    )
+
+
 def print_tabular(formats, sep=" | ", **values):
     layout = sep.join(f"{{{f}}}" for f in formats)
     print(layout.format(**values))
@@ -74,6 +95,12 @@ def print_tabular(formats, sep=" | ", **values):
 
 def print_div():
     print("-" * 60)
+
+
+def print_example(ts):
+    print("example")
+    examples = ts[0]["value"]
+    pprint.pprint(examples[math.floor(len(examples)/2)])
 
 
 time_stat_formats = (
@@ -110,6 +137,7 @@ def print_history_timing_table(provinces, repeats=1, delay=10):
         "provinces!s:<5",
         "group_vars_in_database!s:<15",
         "compact!s:<8",
+        "include_uri!s:<8",
         *time_stat_formats,
         "item_count!s:>10",
     )
@@ -121,24 +149,25 @@ def print_history_timing_table(provinces, repeats=1, delay=10):
         formats,
         compact="compact",
         group_vars_in_database="grp vars in db",
+        include_uri="incl uri",
         provinces="prov",
         **time_stat_titles,
         item_count="# items",
     )
-    for group_vars_in_database in (True, False):
-        for compact in (False, True):
-            args = dict(
-                compact=compact,
-                group_vars_in_database=group_vars_in_database,
-                provinces=provinces,
-            )
-            ts = timing(histories.list, repeats=repeats, delay=delay, **args)
-            print_tabular(
-                formats,
-                **args,
-                **time_stat_values(ts),
-                item_count=len(ts[0]["value"]),
-            )
+    for nv in dict_iter(
+        ("group_vars_in_database", boolean),
+        ("compact", boolean),
+        ("include_uri", boolean),
+    ):
+        args = {**nv, "provinces": provinces}
+        ts = timing(histories.list, repeats=repeats, delay=delay, **args)
+        print_tabular(
+            formats,
+            **args,
+            **time_stat_values(ts),
+            item_count=len(ts[0]["value"]),
+        )
+        # print_example(ts)
     print_div()
 
 
@@ -163,23 +192,19 @@ def print_station_timing_table(provinces, repeats=1, delay=10):
         **time_stat_titles,
         item_count="# items",
     )
-    for group_vars_in_database in (False, True):
-        for compact in (False, True):
-            for expand in ("histories", None):
-                # for expand in (None, "histories"):
-                args = dict(
-                    compact=compact,
-                    group_vars_in_database=group_vars_in_database,
-                    provinces=provinces,
-                    expand=expand,
-                )
-                ts = timing(stations.list, repeats=repeats, delay=delay, **args)
-                print_tabular(
-                    formats,
-                    **args,
-                    **time_stat_values(ts),
-                    item_count=len(ts[0]["value"]),
-                )
+    for nv in dict_iter(
+        ("group_vars_in_database", boolean),
+        ("compact", boolean),
+        ("expand", ("histories", None)),
+    ):
+        args = {**nv, "provinces": provinces}
+        ts = timing(stations.list, repeats=repeats, delay=delay, **args)
+        print_tabular(
+            formats,
+            **args,
+            **time_stat_values(ts),
+            item_count=len(ts[0]["value"]),
+        )
     print_div()
 
 
