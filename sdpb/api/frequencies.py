d0f9974bc27c2a9525c518d9e3f1fb65bbc79f70
rod-glover <rod.glover.ca@gmail.com>
2022-08-11 15:21:35 -0700

    Add prov qp to /frequencies

--- sdpb/api/frequencies.py
+++ sdpb/api/frequencies.py
@@ -1,10 +1,11 @@
 from pycds import History
 from sdpb import get_app_session
 from sqlalchemy import distinct
+from sdpb.util.query import add_province_filter
 
 
-def collection():
-    frequencies = (
-        get_app_session().query(distinct(History.freq).label("freq")).all()
-    )
+def collection(provinces=None):
+    q = get_app_session().query(distinct(History.freq).label("freq"))
+    q = add_province_filter(q, provinces)
+    frequencies = q.all()
     return [f.freq for f in frequencies]
