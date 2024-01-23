974fb50db026ee61b2caed38ddc5bea9164b744c
John <johnsampson@uvic.ca>
2024-01-23 00:24:52 +0000

    \#76 Add prototype for station observation data endpoint
    - Update API spec
    - Add implementation for endpoint
    - Add launch.json and .env.example for environment settings.

--- sdpb/api/stations.py
+++ sdpb/api/stations.py
@@ -22,9 +22,10 @@ Stations returned are always filtered by:
 - Matches province filter (collection)
 """
 import logging
+import datetime
 from flask import url_for
 from sqlalchemy import func
-from pycds import Station, History, StationObservationStats
+from pycds import Station, History, StationObservationStats, Obs, Variable
 from sdpb import get_app_session
 from sdpb.api import networks
 from sdpb.api import histories
@@ -283,3 +284,120 @@ def collection(
                 compact=compact,
                 expand=expand,
             )
+
+
+####
+# /stations/{station_id}/observations/{var_id}
+####
+
+
+def observations_span_uri(station_id, var_id, start_date=None, end_date=None):
+    return url_for(
+        "sdpb_api_stations_get_observations",
+        station_id=station_id,
+        var_id=var_id,
+        start_date=start_date,
+        end_date=end_date,
+    )
+
+
+def obs_values_by_station_query(
+    session, station_id, var_id, end_date: datetime, start_date: datetime
+):
+    """
+    Return a query for observations over a range. The query can include filters
+    for start and end date, stations of interest, and province(s) of interest.
+
+    :param session: SQLAlchemy db session
+    :param station_id: (int) Station id
+    :param var_id: (int) Variable id
+    :param start_date: (datetime) Start date for observation counts.
+    :param end_date: (datetime) End date for observation counts.
+    :return: SQLAchemy query object
+    """
+
+    # Fundamental query: Sum observation counts by month and history id over history
+    # id's for each station, yielding counts per station.
+    q = (
+        session.query(Obs)
+        .filter(Variable.id == var_id)
+        .filter(History.station_id == station_id)
+        .join(History)
+        .join(Variable)
+    )
+
+    if start_date:
+        q = q.filter(Obs.time >= func.date_trunc("day", start_date))
+
+    if end_date:
+        q = q.filter(Obs.time <= func.date_trunc("day", end_date))
+
+    return q
+
+
+def get_observations(station_id, var_id, start_date=None, end_date=None):
+    """
+    Return a dict containing observations for a station and variable over a range.
+
+    if no end_date is specified, todays date will be assumed.
+    if no start_date is specified, the last 26 weeks will be assumed.
+    if start_date is specified, it must be within the last 26 weeks as a protection against this query being used to
+    return too much data.
+
+    :param station_id: (int) Station id
+    :param var_id: (int) Variable id
+    :param start_date: (datetime) Start date for observations.
+    :param end_date: (datetime) End date for observations.
+    :return: dict
+    """
+    assert station_id is not None, "station_id must be specified"
+    assert var_id is not None, "var_id must be specified"
+
+    start_date_obj = datetime.fromisoformat(start_date) if start_date else None
+    end_date_obj = datetime.fromisoformat(end_date) if end_date else None
+
+    if end_date_obj is None:
+        # TODO: this could be set to the last date that the station has data for
+        end_date_obj = datetime.date.today()
+
+    if (
+        start_date_obj is None
+        or start_date_obj > end_date_obj
+        or start_date_obj < (end_date_obj - datetime.timedelta(weeks=26))
+    ):
+        start_date_obj = end_date_obj - datetime.timedelta(weeks=26)
+
+    session = get_app_session()
+
+    obs_vals_by_station = obs_values_by_station_query(
+        session,
+        station_id=station_id,
+        var_id=var_id,
+        start_date=start_date_obj,
+        end_date=end_date_obj,
+    ).all()
+
+    station = session.query(Station).filter(Station.id == station_id).one()
+    variable = session.query(Variable).filter(Variable.id == var_id).one()
+
+    return {
+        "uri": observations_span_uri(
+            station_id, var_id, start_date=start_date, end_date=end_date
+        ),
+        "start_date": start_date_obj,
+        "end_date": end_date_obj,
+        "station": {
+            "id": station.id,
+            "uri": url_for("sdpb_api_stations_single", id=station.id),
+            "network_uri": networks.uri(station.network),
+        },
+        "variable": {
+            "id": variable.id,
+            "uri": url_for("sdpb_api_variables_single", id=variable.id),
+            "name": variable.display_name,
+            "unit": variable.unit,
+        },
+        "observations": [
+            {"value": o.datum, "time": o.time} for o in obs_vals_by_station
+        ],  # list of observations
+    }
